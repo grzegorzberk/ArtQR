2f0a369a7a0c3f2cf5fbc7b4aa6fdffc34718c3b
grzegorzberk <77812797+grzegorzberk@users.noreply.github.com>
2024-07-02 19:22:12 +0200

    Update QRScannerViewModel.swift
    fixed code

--- ArtQR/ModelView/QRScannerViewModel.swift
+++ ArtQR/ModelView/QRScannerViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class QRScannerViewModel: ObservableObject {
+    @Published var scannedCode: String?
+    
+    func handleScannedCode(_ code: String?) {
+        DispatchQueue.main.async { // Zaktualizuj na głównym wątku
+                    self.scannedCode = code
+                }
+    }
+    
+    func handleScannerError(_ error: Error) {
+        // Obsługa błędów skanowania
+        print("Błąd skanowania: \(error.localizedDescription)")
+    }
+    
+}
+
+extension QRScannerViewModel: ScannerCoordinatorDelegate {
+    func didFindCode(_ code: String) {
+        handleScannedCode(code)
+    }
+    
+    func didFailWithError(_ error: Error) {
+        handleScannerError(error)
+    }
+}
