e2f48a2d00703bd07fa0d716c1d8b67f1fed239e
grzegorzberk <77812797+grzegorzberk@users.noreply.github.com>
2024-07-02 19:22:52 +0200

    Update ScannerCoordinator.swift
    fixed code

--- ArtQR/QRScanner/ScannerCoordinator.swift
+++ ArtQR/QRScanner/ScannerCoordinator.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import AVFoundation
+
+class ScannerCoordinator: NSObject {
+    weak var delegate: ScannerCoordinatorDelegate?
+    private let metadataOutput = AVCaptureMetadataOutput()
+
+    init(delegate: ScannerCoordinatorDelegate) {
+        self.delegate = delegate
+        super.init()
+    }
+
+    func startScanning() {
+        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+    }
+}
+
+extension ScannerCoordinator: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+           let stringValue = metadataObject.stringValue {
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            delegate?.didFindCode(stringValue)
+        }
+    }
+}
