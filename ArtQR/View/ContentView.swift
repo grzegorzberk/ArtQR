8085c5c7c4af072e8b0ecc584c880f6019db4747
grzegorzberk <77812797+grzegorzberk@users.noreply.github.com>
2024-07-02 19:24:04 +0200

    Update ContentView.swift
    fixed code

--- ArtQR/View/ContentView.swift
+++ ArtQR/View/ContentView.swift
@@ -5,4 +5,32 @@
 //  Created by Grzegorz Berk on 21/04/2024.
 //
 
-import Foundation
+import SwiftUI
+import CodeScanner
+
+struct ContentView: View {
+    @StateObject private var viewModel = QRScannerViewModel()
+    @State private var isShowingScanner = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                if let code = viewModel.scannedCode {
+                    DisplayInfoView(code: code, dismissAction: {
+                        viewModel.scannedCode = nil // Zresetuj zeskanowany kod po powrocie z widoku informacji
+                    })
+                } else {
+                    Button("Skanuj kod QR") {
+                        isShowingScanner = true
+                    }
+                    .padding()
+                }
+            }
+            .navigationBarTitle("Odczytywanie QR")
+        }
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(delegate: viewModel, codeTypes: [.qr])
+        }
+    }
+}
+
